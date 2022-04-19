d94a158897d15708ebe872e45fe109932042d7db
Tecnika <nika.koledova@yandex.ru>
2022-04-19 16:50:32 +0300

    Add files via upload

---
+++ icons.js
@@ -0,0 +1,49 @@
+let files = [{
+        name: "access-point.svg",
+        pack: [],
+        metatags: [],
+        tags: []
+    },
+    {
+        name: "access-point-check.svg",
+        pack: [],
+        metatags: [],
+        tags: []
+    },
+    {
+        name: "access-point-minus.svg",
+        pack: [],
+        metatags: [],
+        tags: []
+    },
+    {
+        name: "access-point-network.svg",
+        pack: [],
+        metatags: [],
+        tags: []
+    },
+    {
+        name: "access-point-network-off.svg",
+        pack: [],
+        metatags: [],
+        tags: []
+    },
+    {
+        name: "access-point-off.svg",
+        pack: [],
+        metatags: [],
+        tags: []
+    },
+    {
+        name: "access-point-plus.svg",
+        pack: [],
+        metatags: [],
+        tags: []
+    },
+    {
+        name: "access-point-remove.svg",
+        pack: [],
+        metatags: [],
+        tags: []
+    }
+];
---
+++ script.js
@@ -0,0 +1,8 @@
+function generate_card() {
+    let ch1 = '<div class="icon_card vertical "> <img src = "';
+    let ch2 = '"> <p class = "name_card" > ';
+}
+
+function start() {
+
+}
