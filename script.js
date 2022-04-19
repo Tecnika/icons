1335a902c0479a6abc2ccf8e8ad4eaa8e6a14368
Kirill <kirillandreev8888@gmail.com>
2022-04-19 19:23:52 +0300

    да

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function generate_card() {
-    let ch1 = '<div class="icon_card vertical "> <img src = "';
-    let ch2 = '"> <p class = "name_card" > ';
-}
+$(() => {
+	document.getElementById("filepicker").addEventListener(
+		"change",
+		function (event) {
+			let files = event.target.files;
 
-function start() {
-
-}
+			for (let i = 0; i < files.length; i++) {
+				let src = files[i].webkitRelativePath;
+				$('.container_list').append(
+					`
+                    <div class="search_block container">
+                        <div class="icon_visible horisontal">
+                            <img src="${src}">
+                        </div>
+                        <div class="tag_table vertical">
+                            <div class="packs group"> 1</div>
+                            <div class="metatags group">2 </div>
+                            <div class="tags group"> 3</div>
+                        </div>
+                    </div>
+                    `
+				);
+			}
+		},
+		false
+	);
+});
